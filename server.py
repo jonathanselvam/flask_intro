d31ea1c65353756dfa1dc8b482b1c88c9d92dbf7
jonathanselvam <jonathan.selvam@gmail.com>
2019-11-07 11:05:55 +0800

    initial commit

--- server.py
+++ server.py
@@ -1,18 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # Revisit decorators if you unclear of this syntax
 def index():
-    return render_template('index.html') # by default looks for index.html inside a templates folder in the same directory as this script.
+    return '<h1>Why so easy</h1>'
 
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
-
-@app.route('/user/<username>')
-def profile(username): # cannot repeat function name ie "show"
-    # username = 'Jake'
-    return f"Hi {username}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
+   app.run()
